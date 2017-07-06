93088f226d768c5c9a74afcf6415febfb1c33d05
bloctuary <doctuary@github.com>
2017-07-06 07:32:12 -0500

    FST clean-up conventions

--- src/server/init-store.js
+++ src/server/init-store.js
@@ -10,11 +10,11 @@ const initStore = (plainPartialState: ?Object) => {
   const preloadedState = plainPartialState ? {} : undefined
 
   if (plainPartialState && plainPartialState.hello) {
-    //flow-disable-next-line
+    // flow-disable-next-line
     preloadedState.hello = helloReducer(undefined, {})
       .merge(Immutable.fromJS(plainPartialState.hello))
   }
-  
+
   return createStore(combineReducers({ hello: helloReducer }),
     preloadedState, applyMiddleware(thunkMiddleware))
 }
